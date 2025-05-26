0c39d47e67c01a377ad4d0cf0abbb681fd7357b4
sumitraay2022@gmail.com <sumitraay2022@gmail.com>
2025-05-26 14:22:37 +0545

    Initial commit with both React and Django

---
+++ Backend/api/serializers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True,  
+        min_length=8,
+        required=True
+    )
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "password"]  
+
+    def create(self, validated_data):  
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"]
+        )
+        return user
---
+++ onlinebiddingsystem/README.md
@@ -0,0 +1,5 @@
+# Online Bidding System
+
+![React CI/CD](https://github.com/sumit4622/onlinebiddingsystem/actions/workflows/deploy.yml/badge.svg)
+
+A web-based platform for online bidding built with React.
