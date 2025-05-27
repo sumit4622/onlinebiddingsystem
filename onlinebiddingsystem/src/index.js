beb0837cf495cbbb8d0098eb2831ec81e0176714
sumitraay2022@gmail.com <sumitraay2022@gmail.com>
2025-05-27 14:26:22 +0545

    added Hero section in landing page

--- onlinebiddingsystem/src/index.js
+++ onlinebiddingsystem/src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // or bootstrap.min.jss
 
-import { Header } from './landingPage/Components/header.tsx';
+import  Home  from './landingPage/Home.tsx';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <App />
+    <Home />
     
   </React.StrictMode>
 );
